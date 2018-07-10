6aa98ff9677a0e5976feb3777c237c55afa0a686
rajewski <rajewski23@gmail.com>
2018-07-09 23:06:19 -0700

    add several analysis scripts

--- analysis/sleuth.R
+++ analysis/sleuth.R
@@ -8,6 +8,7 @@ devtools::install_github("pachterlab/sleuth")
 library(sleuth)
 library(dplyr)
 library(ggplot2)
+library(cowplot) #for pretty graphs
 #make experiemental information matrix from scratch rather than an import
 s2c <- read.table("../replist.txt", header=F, stringsAsFactors = F)
 colnames(s2c) <- "sample"
@@ -23,7 +24,7 @@ s2c
 #I am going to try to do both  and see if it differs
 library(splines)
 day <- s2c$apprDPA
-full_design <- model.matrix(formula(~ ns(day, df = 4)))
+full_design <- model.matrix(formula(~ ns(day, df = 3)))
 
 #construct sleuth object or read it if you have already done this
 #so <- readRDS("Sleuth_so.rds")
@@ -40,12 +41,13 @@ plot_qq(so, test = 'reduced:full', test_type = 'lrt', sig_level = 0.05)
 lrt_results <- sleuth_results(so, 'reduced:full', test_type = 'lrt')
 #"true" means these transcripts' expression is better explained by a spline than noise
 table(lrt_results[,"qval"] < 0.05) 
-
+head(lrt_results[lrt_results$qval<0.05,])
 #Who's on top?
 head(lrt_results, n=20)[,1:3]
 
 #plot a heatmap
-plot_transcript_heatmap(so, head(lrt_results, n = 20)$target_id, 'est_counts')
+plot_transcript_heatmap(so, head(lrt_results[lrt_results$qval<0.05,], n=100)$target_id, cluster_transcripts = TRUE, 'est_counts')
+
 
 #plot a single DE transcript
 tmp <- so$obs_raw %>% dplyr::filter(target_id == 'Solyc06g069430.3.1') #FUL1 (FUL2=Solyc03g114830.3.1)
@@ -57,3 +59,56 @@ ggplot(tmp, aes(x=day, y=est_counts)) + geom_point(shape=1) + geom_smooth(method
 
 #try shiny
 sleuth_live(so)
+
+
+###You must run the glimma script before you can do this analysis. It depends on files made by that script
+#try to make a plot of a cluster of genes
+#get a list of expression for each gene
+library(tidyr)
+data <- so$obs_raw[,c(1,2,6)] %>% spread(sample,est_counts)
+rownames(data) <- data[,1] #give row names
+data <- data[,-1] #drop names
+data <- data[,c(4:9,1:3,10:15)] #reorder columns by time
+
+#subset this by cluster
+clust1 <- read.delim("DEGs_filtered15_Clust1GENE",header=F)
+clust1 <- data[clust1$V1,]
+tmp <- so$obs_raw[so$obs_raw$target_id %in% clust1$V1,] 
+tmp <- dplyr::full_join(so$sample_to_covariates, tmp, by = 'sample')
+tmp #its expression
+tmp <- tmp[,c("apprDPA","target_id","est_counts")]
+tmp <- aggregate(est_counts~apprDPA+target_id,tmp,mean)
+#make actual graph
+ggplot(tmp, aes(x=apprDPA, y=est_counts, group=target_id)) + geom_line()
+
+#try to get them all at once
+clusters <- read.delim("DEGs_filtered_pam15reordered.txt")
+clusters <- clusters[,c("X","clust.sort")]
+colnames(clusters) <- c("target_id","clust.sort")
+expr <- dplyr::full_join(so$sample_to_covariates, so$obs_raw, by = 'sample')
+expr <- expr[,c("apprDPA","target_id","est_counts")]
+expr <- aggregate(est_counts~apprDPA+target_id,expr,mean)
+expr <- dplyr::full_join(clusters,expr, by = "target_id")
+exprsumm <- aggregate(est_counts~apprDPA+clust.sort, expr, mean)
+
+#Make Graph itself
+pdf(file = "ClusterProfiles.pdf", width=14, height=10)
+ggplot(expr %>% drop_na(), aes(x=apprDPA, y=est_counts, group=target_id)) + 
+  ylab("Estimated Counts") +
+  xlab("Days Post-Anthesis") +
+  geom_line() +
+  facet_wrap(~clust.sort, ncol=5, scales="free")
+dev.off()
+
+pdf(file="ClusterProfilesBasic.pdf", width=14, height=10)
+ggplot(data=exprsumm %>% drop_na(), aes(x=apprDPA, y=est_counts)) + 
+  ylab("Estimated Counts") +
+  xlab("Days Post-Anthesis") +
+  ggtitle("Clustered Expression Profiles") +
+  geom_line() +
+  facet_wrap(~clust.sort, ncol=5, scales="free")
+dev.off()
+
+
+
+
