3ee5124f0cd98724114bc2c7bd2768a3b9b9aed5
rajewski <rajewski23@gmail.com>
2019-07-21 11:06:32 -0700

    add ballgown and edgeR script

---
+++ analysis/Ballgown_edgeR.R
@@ -0,0 +1,3 @@
+#This is an analysis script for the data that is output by Ballgown (mapped by stringtie).
+#This is not to be confused with the analysis done by Kallisto, which is not my current method
+
