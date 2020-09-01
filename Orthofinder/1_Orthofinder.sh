e64b5b27c13ac1a1bb39c0b557cf756763deb697
rajewski <rajewski23@gmail.com>
2020-08-31 17:18:19 -0700

    melon orthos

--- Orthofinder/1_Orthofinder.sh
+++ Orthofinder/1_Orthofinder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=60
+#SBATCH --cpus-per-task=30
 #SBATCH --mem-per-cpu=7G
 #SBATCH --nodes=1
 #SBATCH --time=1-00:00:00
@@ -13,13 +13,12 @@ set -e
 ln -sf ../ExternalData/TAIR10/TAIR10.proteins.fa Arabidopsis.fa
 ln -sf ../SlycDNA/Slyc.proteins.fa Solanum.fa
 ln -sf ../NobtDNA/NIOBT_r1.0.proteins.fa Nicotiana.fa
+ln -sf ../ExternalData/C_melo/CM3.5.1_protein_longestIsoforms.fa Cucumis.fa
 
-module load orthofinder/2.3.7
+module load orthofinder/2.4.0
 module load mafft/7.427
 module load IQ-TREE/1.6.12
 
 orthofinder \
     -t $SLURM_CPUS_PER_TASK \
-    -M msa \
-    -T iqtree \
     -f ./
