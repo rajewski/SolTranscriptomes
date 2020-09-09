0ad58002804a05b6c178fdf1cf7b9167ac8c54f7
rajewski <rajewski23@gmail.com>
2020-09-09 09:12:04 -0700

    update for 4 genus comparison

--- 6_VennDiagram.R
+++ 6_VennDiagram.R
@@ -1,8 +1,9 @@
 # Orthogroup Venn Diagram -------------------------------------------------
 library("VennDiagram")
+library("dplyr")
 source("X_Functions.R")
 
-Orthos <- read.table("Orthofinder/OrthoFinder/Results_May17/Orthogroups/Orthogroups.tsv",
+Orthos <- read.table("Orthofinder/OrthoFinder/Results_Aug31/Orthogroups/Orthogroups.tsv",
                      sep="\t",
                      stringsAsFactors = F,
                      header=T)
@@ -11,9 +12,10 @@ Orthos <- read.table("Orthofinder/OrthoFinder/Results_May17/Orthogroups/Orthogro
 Set1 <- Orthos[,c(1:2)] %>% filter_all(all_vars(!grepl("^$",.)))
 Set2 <- Orthos[,c(1,3)] %>% filter_all(all_vars(!grepl("^$",.)))
 Set3 <- Orthos[,c(1,4)] %>% filter_all(all_vars(!grepl("^$",.)))
+Set4 <- Orthos[,c(1,5)] %>% filter_all(all_vars(!grepl("^$",.)))
 venn.diagram(
-  x = list(Set1[,1], Set2[,1], Set3[,1]),
-  category.names = c("Arabidopsis", "Nicotiana", "Solanum"),
+  x = list(Set1[,1], Set2[,1], Set3[,1], Set4[,1]),
+  category.names = c("Arabidopsis","Cucumis", "Nicotiana", "Solanum"),
   filename = "Figures/AllOrthogroups.png",
   imagetype = "png",
   main="Orthogenes",
@@ -26,17 +28,17 @@ venn.diagram(
   euler.d=T,
   output=F,
   lwd=2,
-  lty=1,
-  fill=palw[2:4])
+  lty=1)
 
 # All Species, single copy
 SingleOrthos <- Orthos %>% filter_all(all_vars(!grepl(',',.))) #Remove multiples
 SingleSet1 <- SingleOrthos[,c(1:2)] %>% filter_all(all_vars(!grepl("^$",.)))
 SingleSet2 <- SingleOrthos[,c(1,3)] %>% filter_all(all_vars(!grepl("^$",.)))
 SingleSet3 <- SingleOrthos[,c(1,4)] %>% filter_all(all_vars(!grepl("^$",.)))
+SingleSet4 <- SingleOrthos[,c(1,5)] %>% filter_all(all_vars(!grepl("^$",.)))
 venn.diagram(
-  x = list(SingleSet1[,1], SingleSet2[,1], SingleSet3[,1]),
-  category.names = c("Arabidopsis", "Nicotiana", "Solanum"),
+  x = list(SingleSet1[,1], SingleSet2[,1], SingleSet3[,1], SingleSet4[,1]),
+  category.names = c("Arabidopsis","Cucumis", "Nicotiana", "Solanum"),
   filename = "Figures/SingleCopyOrthogroups.png",
   imagetype = "png",
   main="Shared Single-Copy Orthogenes",
@@ -49,33 +51,6 @@ venn.diagram(
   euler.d=T,
   output=F,
   lwd=2,
-  lty=1,
-  fill=palw[2:4])
-
-# Dry Species Only
-DryOrthos <- SingleOrthos[,1:3] #For just Dry Species
-DrySet1 <- DryOrthos[,c(1:2)] %>% filter_all(all_vars(!grepl("^$",.)))
-DrySet2 <- DryOrthos[,c(1,3)] %>% filter_all(all_vars(!grepl("^$",.)))
-
-venn.diagram(
-  x = list(DrySet1[,1], DrySet2[,1]),
-  category.names = c("  Arabidopsis", "Nicotiana  "),
-  filename = "Figures/DryOrthogroups.png",
-  imagetype = "png",
-  main="Shared Single-Copy Orthogenes",
-  main.fontfamily = "sans",
-  main.fontface = "bold",
-  sub="Dry-Fruited Species",
-  sub.fontfamily = "sans",
-  sub.fontface = "plain",
-  cat.fontface="italic",
-  cat.fontfamily="sans",
-  fontfamily="sans",
-  scaled=T,
-  euler.d=T,
-  output=F,
-  lwd=2,
-  lty=1,
-  fill=palw[2:3])
+  lty=1)
 
 
