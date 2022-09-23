58b270260005b8f8ddbff7c37dd31b4e0562f824
rajewski <rajewski23@gmail.com>
2022-09-23 16:39:07 -0700

    add SRA download script for Raw data

---
+++ 1_SRADownload.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --nodes=1
+#SBATCH --mail-user=rajewski23@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH -o ./logs/SRADownload-%A.out
+set -eu
+
+# Load SRA Toolkit
+module load sratoolkit/3.0.0
+
+# Get vars based on array ID
+accession=$(awk "NR==$SLURM_ARRAY_TASK_ID" SRA_IDs.tsv | cut -f1)
+stem=$(awk "NR==$SLURM_ARRAY_TASK_ID" SRA_IDs.tsv | cut -f2)
+
+# Get FASTQs
+echo "Dumping FASTQ(s) for $accession with name $stem"
+fastq-dump \
+	--defline-seq '@$sn[_$rn]/$ri' \
+	--defline-qual '+$sn[_$rn]/$ri' \
+	--split-3 \
+	--gzip \
+	-B \
+	-O ./SRA/ \
+	$accession
+
+# rename the file
+# Thanks: https://stackoverflow.com/a/40029320/13954432
+find . -type f | sed -n "s/$accession1\(_[1|2]\).fastq.gz/& Test\1.fastq.gz/p" | xargs -n 2 mv
